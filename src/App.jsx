a2c3b171f049dff230dcf7dd4490c324c458327b
morrisco <morriscotech@gmail.com>
2025-07-18 02:07:07 +0100

    bootstrap update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React,  { useState } from 'react'
-import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '/node_modules/bootstrap/dist/js/bootstrap.min.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
 
 // internal url
