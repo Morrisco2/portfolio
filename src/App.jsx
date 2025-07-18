1b8be12a5a35f76a766fd4670f7bf84df0364a5d
morrisco <morriscotech@gmail.com>
2025-07-18 02:00:52 +0100

    bootstrap update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React,  { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '/node_modules/bootstrap/dist/js/bootstrap.min.js';
+
 
 // internal url
 import Navbar from './Components/Navbar';
